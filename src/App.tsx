f8809847c74b71961260857ec235e8f99c73aee2
Carter <cjwagn1@gmail.com>
2022-04-08 21:15:22 -0500

    Downgraded animation library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import styled, { AnyStyledComponent } from "styled-components";
-
+import { motion } from "framer-motion";
 const Example: AnyStyledComponent = styled.div``;
 
 export default () => {
-  return <Example>This is an example component</Example>;
+  return (<div>
+    <div>
+    <motion.div animate={{ scale: 0.5}} transition={{ duration: 0.5}}>test</motion.div>
+    
+    </div>
+    </div>)
 };
